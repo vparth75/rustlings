071c3541b45058889eafc987d61cb14f3cfe1d57
Parth <vparth75@gmail.com>
2025-09-22 10:18:11 +0530

    day1

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,8 @@
+use std::array;
+
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a = 
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
